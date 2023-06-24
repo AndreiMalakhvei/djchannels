efd7235671305b746cc94c7d0457ec908487b0e5
Andrei Malakhvei <a.malakhvei@gmail.com>
2023-06-24 16:36:56 +0300

    last update

--- chat/firedb/credentials.py
+++ chat/firedb/credentials.py
@@ -1,11 +1,3 @@
 FIRE_CONFIG = {
-    'apiKey': "AIzaSyCd2wvYUpRGOmCxqBRa3YtdjAte62VA0-w",
-    'authDomain': "chat-60128.firebaseapp.com",
-    'projectId': "chat-60128",
-    'storageBucket': "chat-60128.appspot.com",
-    'messagingSenderId': "122065919374",
-    'appId': "1:122065919374:web:424b17a1c85dd3243c18df",
-    'measurementId': "G-4VHP970CEM",
-    'databaseURL': "https://chat-60128-default-rtdb.europe-west1.firebasedatabase.app/",
-    "serviceAccount": "chat-60128-dbs-adminsdk-mjhg2-c740633904.json"
+
 }
